467648c1213b7ba997f3e549835b10733ca7a2d6
herber <herber523@gmail.com>
2017-06-06 22:32:34 +0800

    add util

---
+++ util.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+import numpy as np
+from keras.models import model_from_config
+
+
+
+def clone_model(model, custom_objects={}):
+    config = {
+        'class_name': model.__class__.__name__,
+        'config': model.get_config(),
+    }
+    clone = model_from_config(config, custom_objects=custom_objects)
+    clone.set_weights(model.get_weights())
+    return clone
+
